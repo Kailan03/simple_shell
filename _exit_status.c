ff059ffc4f0c6ead1e12ad34be984bb9d4662668
Ayodelefasulu <fasuluayodele@gmail.com>
2023-11-17 01:52:37 -0800

    fixed _exit_status

--- _exit_status.c
+++ _exit_status.c
@@ -15,26 +15,27 @@ void _exit_status(char *args[], int i)
 {
 	char *input = NULL;
 
-	/* if (i > 1) */
-	/*{*/
-		/* If an argument is provided, try to convert it to an integer */
-		int exit_status;
+	if (i > 1)
+	{
+		char *endptr;
+		int exit_status = strtol(args[1], &endptr, 10);
 
-		if (sscanf(args[1], "%d", &exit_status) == 1)
+		/* if (sscanf(args[1], "%d", &exit_status) == 1) */
 		/* if (strtof32(args[1], 10, &exit_status) == 0) */
+		if (*endptr == '\0')
 		{
-		/* printf("Exiting the shell with status: %d\n", exit_status); */
+			printf("Exiting the shell with status: %d\n", exit_status);
 			free(input); /* Free allocated memory */
 			exit(exit_status); /* Terminate the shell with the specified status */
 		}
 		else
 		{
-			perror(__progname);
-			fprintf(stderr, "%s: %d: exit: Illegal number: %s\n",
-					__progname, i, args[1]);
-		/* printf("Invalid exit status. Exiting the shell with status 1.\n"); */
+			/* perror(__progname); */
+			/* fprintf(stderr, "%s: %d: exit: Illegal number: %s\n", */
+					/* __progname, i, args[1]); */
+			printf("Invalid exit status. Exiting the shell with status 1.\n");
 			free(input); /* Free allocated memory */
 			exit(1); /* Default to exit with status 1 */
 		}
-	/*}*/
+	}
 }
